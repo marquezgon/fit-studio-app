a3acd560e1860825f708463a072075497ba67c5b
Gonzalo <gonzalo.marquez@projectindigo.io>
2023-11-08 09:58:47 -0600

    changed zeal title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Zeal Studio | Join the Movement',
+  title: 'Zeal Studio',
   description: 'Plataforma Online de Zeal Studio',
 }
 

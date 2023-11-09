228f65a3e22e9d22031f3421673bf065aecfea90
Gonzalo <gonzalo.marquez@projectindigo.io>
2023-11-09 13:05:43 -0600

    added profile page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -97,7 +97,7 @@ export default function Profile() {
           </TableHeader>
           <TableBody>
             {classesAndPackages.userPackages.map((pack) => (
-              <TableRow key={pack.id}>
+              <TableRow key={pack.package_id}>
                 <TableCell>
                   <p className='text-xs md:text-base text-black uppercase'>
                     {pack.amount} {`${pack.amount == 1 ? 'class' : 'classes'} pack`}
